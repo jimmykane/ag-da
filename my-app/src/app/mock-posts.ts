a9892cc50fc386349563f6ffea5cd8528f019475
Dimitrios Kanellopoulos <jimmykane9@gmail.com>
2020-06-07 11:42:17 +0200

    Revert "add image"
    This reverts commit 2f421461318c6581b546c69a8bffe15cb2ce3c00.

--- my-app/src/app/mock-posts.ts
+++ my-app/src/app/mock-posts.ts
@@ -1,8 +1,8 @@
 import { Post } from './post';
 
 export const POSTS: Post[] = [
-  { id: 1, slug: 'slug-1', title: 'Title 1', author: 'Author', description: 'This is a description.', im: "/assets/images/scream.jpg" },
-  { id: 2, slug: 'slug-2', title: 'Title 2', author: 'Author',  description: 'This is a description.', im: "/assets/images/scream.jpg"  },
-  { id: 3, slug: 'slug-3', title: 'Title 3', author: 'Author',  description: 'This is a description.', im: "/assets/images/scream.jpg"  },
-  { id: 4, slug: 'slug-4', title: 'Title 3', author: 'Author',  description: 'This is a description.', im: "/assets/images/scream.jpg"  },
+  { id: 1, slug: 'slug-1', title: 'Title 1', author: 'Author', description: 'This is a description.', im: ".../scream.jpg" },
+  { id: 2, slug: 'slug-2', title: 'Title 2', author: 'Author',  description: 'This is a description.', im: '../scream.jpg'  },
+  { id: 3, slug: 'slug-3', title: 'Title 3', author: 'Author',  description: 'This is a description.', im: '.../scream.jpg'  },
+  { id: 4, slug: 'slug-4', title: 'Title 3', author: 'Author',  description: 'This is a description.', im: '/scream.jpg'  },
 ];
